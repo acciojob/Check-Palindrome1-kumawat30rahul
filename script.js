c5781bdb31fe46a9c62388738691bf47a7f25ee1
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-01-19 18:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,17 @@
 function isPalindrome(s) {
     s = s.toLowerCase();
     s = s.replace(/[^a-z0-9]/g, "");
-    return s === s.split('').reverse().join('');
+    let isPalindrome = true;
+    let left = 0;
+    let right = s.length - 1;
+    s.split('').forEach(function() {
+        if(s[left] !== s[right]) {
+            isPalindrome = false;
+        }
+        left++;
+        right--;
+    });
+    return isPalindrome;
 }
+
 module.exports = palindrome

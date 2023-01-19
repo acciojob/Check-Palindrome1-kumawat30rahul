91f34111f6317393fd8abc74f2b19dbc54f5d3e4
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-01-19 17:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
-function palindrome(str){
-
+function isPalindrome(s) {
+    s = s.toLowerCase();
+    s = s.replace(/[^a-z0-9]/g, "");
+    return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
